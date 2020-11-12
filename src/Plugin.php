1834f2e5deb7fff755ef5c99819ac99620d25fd3
mscherer <mark.scherer@spryker.com>
2020-11-12 13:41:34 +0100

    Add plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Flash;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $consoleEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $bootstrapEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $routesEnabled = false;
+
+}
